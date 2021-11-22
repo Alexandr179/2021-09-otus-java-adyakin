9985e2608634a2b37b8242400917a6d903d6e365
Alexandr <57070103+Alexandr179@users.noreply.github.com>
2021-11-22 14:03:03 +0300

    Fix: cast repo to HW10 only

--- HW06-reflections/src/main/java/ru/otus/annotations/framework/After.java
+++
@@ -1,8 +0,0 @@
-package ru.otus.annotations.framework;
-
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
-
-@Retention(RetentionPolicy.RUNTIME)
-public @interface After {
-}
